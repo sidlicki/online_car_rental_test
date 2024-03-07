e883333b963599a6ce070aebcba52608a687408f
sidlicki <130383232+sidlicki@users.noreply.github.com>
2024-03-07 23:16:10 +0100

    Update CarItem.styled.jsx

--- src/components/CarItem/CarItem.styled.jsx
+++ src/components/CarItem/CarItem.styled.jsx
@@ -4,6 +4,11 @@ export const StyledItem = styled.li`
   display: flex;
   flex-direction: column;
   width: 274px;
+  overflow: hidden;
   .img {
+    width: 100%;
+    height: 100%;
+    min-height: 268px;
+    object-fit: cover;
   }
 `;
